3536634fc3423b35a83a8c89acb62a5c3574d826
Samanta Fluture <samantafluture@gmail.com>
2021-12-29 15:14:21 -0500

    create custom hook for online status

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { useEffect } from 'react';
 import Contador from './components/MeuComponente/Contador';
 import ListaCursos from './components/ListaCursos/ListaCursos';
+import useOnlineStatus from './data/hooks/useOnlineStatus';
 
 function App() {
     const nome = 'Samanta';
+    const isOnline = useOnlineStatus();
+
+    useEffect(() => {
+      if (!isOnline) {
+        alert('Você está offline');
+      }
+    }, [isOnline]);
 
     return (
         <>
@@ -11,6 +20,9 @@ function App() {
                 <h1>Hello World</h1>
             </Contador>
             <ListaCursos />
+            <div>
+                <h2>Você está {isOnline ? 'online' : 'offline'}</h2>
+            </div>
         </>
     );
 }
---
+++ src/data/hooks/useOnlineStatus.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+export default function useOnlineStatus() {
+    const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+    const goOnline = () => setIsOnline(true);
+    const goOffline = () => setIsOnline(false);
+
+    useEffect(() => {
+        window.addEventListener('online', goOnline);
+        window.addEventListener('offline', goOffline);
+
+        return () => {
+            window.removeEventListener('online', goOnline);
+            window.removeEventListener('offline', goOffline);
+        }
+    }, []);
+
+    return isOnline;
+}
